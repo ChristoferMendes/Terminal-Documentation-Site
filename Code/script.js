2d7dfc4393f361d958484f4fa5f999f11b6065b2
Christofer Mendes <christoferluizdsm@gmail.com>
2022-06-22 17:52:40 -0300

    New features

--- Code/script.js
+++ Code/script.js
@@ -35,8 +35,65 @@ const init = function(){
       div2 = cln.textContent;
       
       t2.innerHTML = div2;
-      
+
+      const section2 = document.querySelector(".SectionTwo")
+
+      const nodeList = section2.children;
+
+      let number = nodeList.length;
+
+      const texts = [`${nodeList}`]
+      let count = 0;
+      let index = 0;
+      let currentText = "";
+      let letter = "";
+
+      (function type(){
+
+        if (count === texts.length){
+          count = 0;
+  
+        }
+
+        currentText = texts[count];
+        letter = currentText.slice(0, ++index)
+
+        NodeList.textContent = letter;
+        if (letter.length === currentText.length){
+          count++;
+          index = 0;
+        }
+
+        setTimeout(type, 400)
+      }())
+     
 }
 
 document.addEventListener("DOMContentLoaded", init);
 //dsada
+
+
+
+// let count = 0;
+// let index = 0;
+// let currentText = "";
+// let letter = "";
+
+// (function type(){
+
+//   if(count === texts.length){
+//     count = 0;
+//   }
+//   currentText = texts[count];
+//   letter = currentText.slice(0, ++index);
+//   //Basically, what this do is:
+//   //letter = "w", than -> letter = "we" [...]
+
+//   document.querySelector(".typing").textContent = letter;
+//   if (letter.length === currentText.length){
+//     count++; //Jumping to the next text, because the index of texts is based on this let
+//     index = 0;
+//   }
+//   setTimeout(type, 400)
+// }())
+
